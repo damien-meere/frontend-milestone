e8f27591be65602c6ed9ab026be65409fe296327
Ubuntu <ubuntu@ip-172-31-42-101.us-east-2.compute.internal>
2019-07-12 12:43:30 +0000

    Deletion of unnecessary script from Javascript file

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,56 +9,6 @@ $(document).ready(function() {
 });
 
 
-function contactFormValidation() {
-    var name = document.forms["ContactForm"]["Name0"];
-    var email = document.forms["ContactForm"]["Email0"];
-    var phone = document.forms["ContactForm"]["phoneNumber0"];
-    var msg = document.forms["ContactForm"]["subject0"];
-    var message = document.forms["ContactForm"]["Message0"];
-
-    if (name.value == "") {
-        window.alert("Please enter your name.");
-        name.focus();
-        return false;
-    }
-
-    if (email.value == "") {
-        window.alert("Please enter a valid e-mail address.");
-        email.focus();
-        return false;
-    }
-
-    if (email.value.indexOf("@", 0) < 0) {
-        window.alert("Please enter a valid e-mail address.");
-        email.focus();
-        return false;
-    }
-
-    if (email.value.indexOf(".", 0) < 0) {
-        window.alert("Please enter a valid e-mail address.");
-        email.focus();
-        return false;
-    }
-
-    if (phone.value == "") {
-        window.alert("Please enter your telephone number.");
-        phone.focus();
-        return false;
-    }
-
-    if (msg.value == "") {
-        window.alert("Please enter a Subject");
-        msg.focus();
-        return false;
-    }
-
-    if (message.value == "") {
-        window.alert("Please enter your Message");
-        message.focus();
-        return false;
-    }
-    return true;
-}
 
 /*function to call a specific modal if the URL contains #*/
 $(document).ready(function($) {
